ce03bca3c0a73fc6ac54b14241e6e648034f70e8
Anikey-Pavel <panikey@bk.ru>
2023-05-15 23:06:18 +0300

    HM-6

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 import './style.scss';
-import { asyncFunc } from './async';
-
-// if(console.log) {
-//     console.log(2)
-// } else {
-//     console.log = ...
-//     console.log()
-// }
-
-asyncFunc()
